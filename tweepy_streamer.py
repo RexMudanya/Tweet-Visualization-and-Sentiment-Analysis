c33d8954e89f18137a0e668c672373e3e992c560
RexMudanya <musungumudanya@gmail.com>
2019-07-01 04:33:30 +0300

    added TwitterStreamer class

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -4,21 +4,45 @@ from tweepy import Stream
 
 import twitter_credentials
 
+class TwitterStreamer():
+    '''
+    class for streaming and processing live tweets
+    '''
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        #handles twitter auth and credentials to twitter api
+
+        listener = StdOutListener()
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+
+        stream = Stream(auth, listener)
+
+        stream.filter(track=hash_tag_list)
+
 class StdOutListener(StreamListener):
+    '''
+    Listener class that prints received tweets to stdout 
+    '''
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
 
     def on_data(self, data):
-        print(data)
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on data: %s str(e)")
         return True
     
     def on_error(self, data):
         print(status)
 
 if __name__ == "__main__":
+    
+    hash_tag_list = ['donald trump', 'hillary clinton', 'brnie sanders']
+    fetched_tweets_filename = "tweets.json"
 
-    listener = StdOutListener()
-    auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-    auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-
-    stream = Stream(auth, listener)
-
-    stream.filter(track=['donald trump', 'hillary clinton', 'barnie sanders'])
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
